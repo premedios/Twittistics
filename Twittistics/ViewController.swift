9c0fb809c800202927a28b80402e998d45e485d2
Pedro Remedios <pedro.remedios@gmail.com>
2017-12-13 22:32:39 +0000

    Initial Commit

---
+++ Twittistics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twittistics
+//
+//  Created by Jaime Remedios on 13/12/2017.
+//  Copyright © 2017 Pedro Remedios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
